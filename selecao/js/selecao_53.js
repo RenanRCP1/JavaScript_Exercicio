6372c9c75dc3b498509196828079d907b2f0154e
Renan Camara Pinto <renan_tro2@hotmail.com>
2022-10-23 12:22:55 -0300

    inicio

--- selecao/js/selecao_53.js
+++ selecao/js/selecao_53.js
@@ -1,63 +1,95 @@
-function controllercalculo() {
-    var nota1 = parseFloat(document.getElementById("nota1").value);
-    var nota2 = parseFloat(document.getElementById("nota2").value);
-    var nota3 = parseFloat(document.getElementById("nota3").value);
-    var nota4 = parseFloat(document.getElementById("nota4").value);
-
-    var resultdado = calculoResultado(nota1, nota2, nota3, nota4)
-
-    saidaDados(resultdado);
-
+var aluno = {
+    nota1: 0,
+    nota2: 0,
+    nota3: 0,
+    nota4: 0,
+    media: 0,
+    resultado: 0
 }
 
+!(function () {
+    let alunos = listDadosAluno();
+    montarTabela(alunos);
+})()
 
-function calculoResultado(nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0) {
-    var media = (nota1 + nota2 + nota3 + nota4) / 4;
+function controllerNotas() {
+    aluno.nota1 = parseFloat(document.getElementById("nota-1").value);
+    aluno.nota2 = parseFloat(document.getElementById("nota-2").value);
+    aluno.nota3 = parseFloat(document.getElementById("nota-3").value);
+    aluno.nota4 = parseFloat(document.getElementById("nota-4").value);
+    aluno.media = calculoMedia(aluno.nota1, aluno.nota2, aluno.nota3, aluno.nota4);
+    aluno.resultado = verificarSituacaoAluno(aluno.media);
 
-    if (media >= 5) {
-        return "Aprovado " + media
-    }
-    else {
-        return "reprovado " + media
-    }
+    mostrarResultado(aluno.resultado, aluno.media);
 
+    addDadosAluno(aluno);
+    let alunos = listDadosAluno();
+    montarTabela(alunos);
 }
 
-function saidaDados(texto_saida) {
-    document.getElementById("saidaTexto").innerHTML = texto_saida;
+function calculoMedia(avaliacao1 = 0, avaliacao2 = 0, avaliacao3 = 0, avaliacao4 = 0) {
+    return (avaliacao1 + avaliacao2 + avaliacao3 + avaliacao4) / 4
 }
 
-
-function montarObjeto(numero1, numero2, numero3, numero4, result) {
-    let obj = {
-        nota1: numero1,
-        nota2: numero2,
-        nota3: numero3,
-        nota4: numero4,
-        resultado: result
+function verificarSituacaoAluno(media = 0) {
+    if (media >= 5) {  //ULA -> unidade logica aritmética  
+        return true;
     }
-    salvarDados(obj);
-    recuperarDados();
-    return obj
+    return false;
 }
 
-function salvarDados(obj) {
-    let storage = JSON.parse(localStorage.getItem("calculo"))
-    if (storage == null) {
-        storage = []
+function mostrarResultado(resultado = false, media = 0) {
+    if (resultado) {
+        document.querySelector("#saidaResultado").innerHTML = "Aprovado -> media: " + media.toFixed(2);
+    } else {
+        document.querySelector("#saidaResultado").innerHTML = "Reprovado -> media: " + media.toFixed(2);
     }
-    storage.push(obj);
-
-    localStorage.setItem("calculo", JSON.stringify(storage));
 }
 
-function recuperarDados() {
-    let storage = JSON.parse(localStorage.getItem("calculo"));
-    if (storage != null) {
-        let texto = "";
-        for (var i = 0; i < storage.length; i++) {
-            texto += storage[i].valor1 + " - " + storage[i].valor2 + " - " + storage[i].operacao + " - " + storage[i].resultado + "<br>";
-        }
-        document.getElementById("dados_banco").innerHTML = texto;
+function addDadosAluno(obj = {}) {
+    let dadosBanco = JSON.parse(localStorage.getItem("alunos"));
+    if (!dadosBanco) {
+        dadosBanco = [];
     }
+    dadosBanco.push(obj);
+
+    let jsonObj = JSON.stringify(dadosBanco);
+
+    localStorage.setItem("alunos", jsonObj);
+}
+
+function listDadosAluno() {
+    let dadosBanco = JSON.parse(localStorage.getItem("alunos"));
+    if (!dadosBanco) {
+        dadosBanco = [];
+    }
+    return dadosBanco;
+}
+
+function montarTabela(listDados = []) {
+    let tabela = "<table class='table'>";
+
+    tabela += "<tr>";
+    tabela += "<th>Nota 1</th>";
+    tabela += "<th>Nota 2</th>";
+    tabela += "<th>Nota 3</th>";
+    tabela += "<th>Nota 4</th>";
+    tabela += "<th>Resultado</th>";
+    tabela += "<th>Média</th>";
+    tabela += "</tr>";
+
+    for (var i = 0; i < listDados.length; i++) {
+        tabela += "<tr>";
+        tabela += "<td>" + listDados[i].nota1 + "</td>";
+        tabela += "<td>" + listDados[i].nota2 + "</td>";
+        tabela += "<td>" + listDados[i].nota3 + "</td>";
+        tabela += "<td>" + listDados[i].nota4 + "</td>";
+        tabela += "<td>" + listDados[i].resultado + "</td>";
+        tabela += "<td>" + listDados[i].media + "</td>";
+        tabela += "</tr>";
+    }
+
+    tabela += "</table>";
+
+    document.querySelector("#saidaTabela").innerHTML = tabela;
 }
